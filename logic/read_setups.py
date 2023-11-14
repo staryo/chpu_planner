8208dcdd0a402ea1c509b6210521fbc76723e30c
Артем Столов <artem.stolov@gmail.com>
2023-11-14 09:36:02 +0200

    raport to push in carl

--- logic/read_setups.py
+++ logic/read_setups.py
@@ -1,7 +1,17 @@
+"""
+Читаем наладки
+"""
 from utils.excel import excel_to_dict
 
 
 def read_setups(setups_path, all_equipment_classes, all_operations):
+    """
+    Читаем наладки
+    @param setups_path:str Папка, где лежит файл с наладками
+    @param all_equipment_classes:dict Словарь с классами РЦ
+    @param all_operations:dict Словарь с операциями
+    @return:
+    """
     final_setup = {}
     all_equipment_setups = {}
     for row in excel_to_dict(setups_path):
