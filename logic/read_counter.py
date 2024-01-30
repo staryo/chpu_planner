1af0e1bc3813305b3912d919346c242b90366a63
MikhailAndreichuk <michael15ander@gmail.com>
2024-01-30 20:11:34 +0400

    bug-fix of IndexError when counter_report is empty list

--- logic/read_counter.py
+++ logic/read_counter.py
@@ -8,6 +8,7 @@ from utils.excel import excel_to_dict
 def read_counter(counter_path):
     report = defaultdict(lambda: defaultdict(float))
     for row in excel_to_dict(counter_path):
-        report[row['PHASE_ID']][row['DAY']] += row['QUANTITY']
+        if not str(row['QUANTITY']):
+            report[row['PHASE_ID']][row['DAY']] += row['QUANTITY']
 
     return report
